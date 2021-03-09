336925b26142f7f3f96b2e6da5308603fbb693d6
proman3419 <proman3419@protonmail.com>
2021-03-09 22:01:54 +0100

    Fixed count inversions

--- 2/count_inversions.py
+++ 2/count_inversions.py
@@ -1,5 +1,7 @@
 from math import inf
-def cnt_invs(T, l, r, invs):
+
+
+def cnt_invs(T, l, r, invs=0):
   n = r - l
   if n <= 1:
     return invs
@@ -11,23 +13,37 @@ def cnt_invs(T, l, r, invs):
     return invs
 
   m = (l+r)//2
-  invs += cnt_invs(T, l, m, invs) + cnt_invs(T, m, r, invs)
+  invs = cnt_invs(T, l, m, invs) + cnt_invs(T, m, r, invs)
 
-  L = T[l:m] + [inf] # inf -> warden
-  R = T[m:r] + [inf]
+  L = T[l:m]
+  R = T[m:r]
 
+  len_L = len(L)
+  len_R = len(R)
   i = j = 0
-  for k in range(l, r):
+  k = l
+  while i < len_L and j < len_R:
     if L[i] < R[j]:
       T[k] = L[i]
       i += 1
     else:
       T[k] = R[j]
-      invs += i
-      j += 1    
+      invs += m - l - i
+      j += 1
+    k += 1  
+
+  while i < len_L:
+    T[k] = L[i]
+    i += 1
+    k += 1
+
+  while j < len_R:
+    T[k] = R[j]
+    j += 1
+    k += 1
 
   return invs
 
 
-T = [4, 5, 10, 9]
-print(cnt_invs(T, 0, len(T), 0))
+T = [1, 2, 7, 6, 5, 3, 2]
+print(cnt_invs(T, 0, len(T)))
