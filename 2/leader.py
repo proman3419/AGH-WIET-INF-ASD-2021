84eb19471c70520eb902941a859c88e8749e8316
proman3419 <proman3419@protonmail.com>
2021-03-09 16:49:07 +0100

    Added leader

---
+++ 2/leader.py
@@ -0,0 +1,26 @@
+def leader(A):
+  n = len(A)
+  cnt = 1
+  candidate = A[0]
+  for i in range(1, n):
+    if cnt == 0:
+      cnt = 1
+      candidate = A[i]
+    elif A[i] == candidate:
+      cnt += 1
+    else:
+      cnt -= 1
+
+  cnt = 0
+  for i in range(n):
+    if A[i] == candidate:
+      cnt += 1
+
+  return candidate if cnt >= n/2 else None
+
+
+A = [1, 3, 4, 3, 2, 1, 1] # None
+B = [1, 2, 2, 3, 3, 3, 3, 2, 3] # 3
+
+print(leader(A))
+print(leader(B))
