de49b3dc09a3254900bce6e1159647d40354ee20
proman3419 <proman3419@protonmail.com>
2021-03-27 10:20:20 +0100

    Done zad1, zad2 from kol1_16_17

---
+++ others/kol1_16_17/zad1.py
@@ -0,0 +1,115 @@
+class Node:
+  def __init__(self, val=None, next=None):
+    self.val = val
+    self.next = next
+
+
+def array_to_list(A):
+  head = Node()
+  curr = head
+  for e in A:
+    curr.next = Node(e)
+    curr = curr.next
+
+  return head
+
+
+def display(head):
+  curr = head
+  while curr is not None:
+    print(curr.val, end=' ')
+    curr = curr.next
+  print()
+
+
+##########################################################################
+
+
+def get_ll_len(head):
+  _len = 0
+  while head is not None:
+    _len += 1
+    head = head.next
+
+  return _len
+
+
+def sort_add(head, new):
+  curr = head.next # pomin wartownika
+  if curr is None:
+    head.next = new
+  else:
+    prev = head
+    while curr is not None and curr.val < new.val:
+      prev, curr = curr, curr.next
+
+    prev.next = new
+    new.next = curr
+
+
+def find_bucket(val, n, _min, _max):
+  i = int((val-_min)/(_max-_min)*n)
+  if i == n: i -= 1
+  
+  return i
+
+
+def bucket_sort_ll(head, _min, _max):
+  n = get_ll_len(head)
+  B = [0]*n
+
+  for i in range(n):
+    B[i] = Node()
+
+  curr = head
+  for i in range(n):
+    _next = curr.next
+    curr.next = None
+    sort_add(B[find_bucket(curr.val, n, _min, _max)], curr)
+    curr = _next
+
+  head = curr = B[0]
+  for i in range(1, n):
+    if curr is None:
+      continue
+
+    while curr.next is not None:
+      curr = curr.next
+
+    B[i] = B[i].next # pomin wartownika
+    curr.next = B[i]
+
+  return head.next
+
+
+def min_max_ll(head):
+  if head is None:
+    return (None, None)
+
+  _min = _max = head.val
+  head = head.next
+  while head is not None:
+    if head.val < _min:
+      _min = head.val
+    if head.val > _max:
+      _max = head.val
+    head = head.next
+
+  return (_min, _max)
+
+
+def Sort(list):
+  list = list.next # usuwamy wartownika
+
+  # jezeli lista jest pusta lub jednoelementowa to jest posortowana
+  if list is None or list.next is None:
+    return list
+
+  _min, _max = min_max_ll(list)
+  list = bucket_sort_ll(list, _min, _max)
+
+  return list
+
+
+head = array_to_list([7, 1, 8, 2, -4, -13])
+display(Sort(head))
---
+++ others/kol1_16_17/zad2.py
@@ -0,0 +1,44 @@
+def randomized_partition(A, l, r):
+  x = A[r]
+  i = l - 1
+  for j in range(l, r):
+    if A[j] < x:
+      i += 1
+      A[i], A[j] = A[j], A[i]
+
+  i += 1
+  A[i], A[r] = A[r], A[i]
+
+  return i
+
+
+def randomized_select(A, l, r, i):
+  if l == r:
+    return A[l]
+
+  pi = randomized_partition(A, l, r)
+  if i == pi:
+    return A[pi]
+  elif i < pi:
+    return randomized_select(A, l, pi-1, i)
+  else:
+    return randomized_select(A, pi+1, r, i)
+
+
+def SumBetween(T, _from, to, n):
+  if n == 0:
+    return 0
+
+  _from_val = randomized_select(T, 0, n-1, _from)
+  to_val = randomized_select(T, 0, n-1, to)
+
+  _sum = 0
+  for i in range(n):
+    if _from_val <= T[i] <= to_val:
+      _sum += T[i]
+
+  return _sum
+
+
+T = [1, 7, 2, 3, 6, 5]
+print(SumBetween(T, 2, 4, len(T)))
