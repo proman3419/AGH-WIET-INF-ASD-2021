788317d050175cd4254cdced4ac96cb2ffec6ef0
proman3419 <proman3419@protonmail.com>
2021-05-04 19:44:45 +0200

    Added printing solution

--- 6/black_forest.py
+++ 6/black_forest.py
@@ -1,3 +1,34 @@
+def print_solution(F, c, profit):
+  n = len(c)
+  res = []
+
+  i = n - 1
+  while i > 0 and F[i] == F[i-1]:
+    i -= 1
+  res.append(c[i])
+
+  while i >= 0:
+    j = i
+    # szukamy kandydata
+    while j >= 0 and F[j] + c[i] != F[i]:
+      j -= 1
+
+    if j < 0:
+      break
+
+    # pomijamy tych o tej samej wartosci w F co kandydat,
+    # szukany bedzie pierwszym z ta wartoscia
+    while j > 0 and F[j] == F[j-1]:
+      j -= 1
+
+    res.append(c[j])
+    i = j
+
+  for i in range(len(res)-1, -1, -1):
+    print(res[i], end=' ')
+  print(f'\n{profit}')
+
+
 def black_forest(c):
   n = len(c)
   if n == 1: return c[0]
@@ -11,13 +42,17 @@ def black_forest(c):
   for i in range(2, n):
     F[i] = max(F[i-1], F[i-2] + c[i])
 
-  return F[-1]
+  return (F[-1], F)
 
 
 # 1100
 # c = [100, 1000, 1, 100]
 
 # 21
-c = [7, 2, 1, 11, 4, 3]
+# c = [7, 2, 1, 11, 4, 3, 1]
 
-print(black_forest(c))
+# 161
+c = [56, 2, 14, 3, 1, 91]
+
+profit, F = black_forest(c)
+print_solution(F, c, profit)
