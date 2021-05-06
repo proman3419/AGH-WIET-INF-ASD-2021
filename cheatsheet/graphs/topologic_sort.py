9723b0c8f461756d3c440605500c7f18bf782294
proman3419 <proman3419@protonmail.com>
2021-05-06 16:42:25 +0200

    Finished topologic sort

---
+++ cheatsheet/graphs/topologic_sort.py
@@ -0,0 +1,44 @@
+# algorytm zaklada, ze sortowany graf jest DAGiem (Directed Acyclic Graph)
+
+
+def topologic_sort_util(graph, s, visited, res, m):
+  def dfs_visit(u):
+    nonlocal graph, visited, res, m
+
+    visited[u] = True
+    for v in graph[u]:
+      if not visited[v]:
+        #print(f'odwiedz: {v}')
+        dfs_visit(v)
+
+    res[m] = u
+    m -= 1
+
+  #print(f'odwiedz: {s}')
+  dfs_visit(s)
+
+  return m
+
+
+def topologic_sort(graph):
+  n = len(graph)
+  visited = [False]*n
+  res = [-1]*n
+  m = n - 1 # ostatni indeks, do ktorego bedziemy wpisywali rozwiazanie
+
+  for i in range(n):
+    if not visited[i]:
+      m = topologic_sort_util(graph, i, visited, res, m)
+
+  return res
+
+
+graph = [[1, 2],
+         [2, 4],
+         [],
+         [],
+         [3, 5, 6],
+         [],
+         []]
+
+print(topologic_sort(graph))
