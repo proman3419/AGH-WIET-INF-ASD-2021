856f97d2bbb050fd8d79112574cc808e8d66dc82
proman3419 <proman3419@protonmail.com>
2021-05-16 14:32:13 +0200

    Added weighted graphs support in change_graph_format

--- cheatsheet/graphs/change_graph_format.py
+++ cheatsheet/graphs/change_graph_format.py
@@ -21,6 +21,31 @@ def am_to_nl(am):
   return nl
 
 
+# [[(neighbor, weight), ...], ...]
+def nl_to_am_w(nl):
+  n = len(nl)
+  am = [[0 for _ in range(n)] for _ in range(n)]
+
+  for i in range(n):
+    for j in range(len(nl[i])):
+      am[i][nl[i][j][0]] = nl[i][j][1]
+
+  return am
+
+
+# 0 - brak krawedzi, >= 1 - waga krawedzi
+def am_to_nl_w(am):
+  n = len(am)
+  nl = [[] for _ in range(n)]
+
+  for i in range(n):
+    for j in range(n):
+      if am[i][j] > 0:
+        nl[i].append((j, am[i][j]))
+
+  return nl
+
+
 graph = [[1, 4],
          [2, 3],
          [0, 7],
@@ -33,8 +58,21 @@ graph = [[1, 4],
          [10],
          [8]]
 
-graph = [[1, 2], [0, 2], [0, 1, 8], [4, 5], [3, 6], [3, 6, 10], [4, 5, 7], [6, 8, 9], [2, 7, 9], [7, 8], [5, 11, 12], [10, 12], [10, 11]]
+# graph = []
 
-print(nl_to_am(graph))
-#print(am_to_nl(graph))
-#print(am_to_nl(nl_to_am(graph)))
+# print(nl_to_am(graph))
+# print(am_to_nl(graph))
+# print(am_to_nl(nl_to_am(graph)))
+
+graph_w = [[(1, 2), (2, 4)],
+           [(2, 1), (3, 7)],
+           [(4, 3)],
+           [(5, 1)],
+           [(3, 2), (5, 5)],
+           []]
+
+# graph_w = []
+
+print(nl_to_am_w(graph_w))
+# print(am_to_nl_w(graph_w))
+# print(am_to_nl_w(nl_to_am_w(graph_w)))
