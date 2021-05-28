bb969094787d680b894dc4a86d83807489863f5c
proman3419 <proman3419@protonmail.com>
2021-05-28 19:06:30 +0200

    Added verification to bellman-ford

--- cheatsheet/graphs/bellman-ford.py
+++ cheatsheet/graphs/bellman-ford.py
@@ -1,6 +1,22 @@
 from math import inf
 
 
+# True - posiada ujemny cykl
+# False - nie
+def verification(graph, distances):
+  n = len(graph)
+  for u in range(n):
+    for v, w in graph[u]:
+      if distances[v] > distances[u] + w:
+        return True
+    # for v in range(n):
+    #   if graph[u][v] != 0:
+    #     if distances[v] > distances[u] + graph[u][v]:
+    #       return True
+
+  return False
+
+
 def bellman_ford(graph, s):
   n = len(graph)
   distances = [inf]*n
@@ -10,16 +26,18 @@ def bellman_ford(graph, s):
 
   for i in range(n):
     for u in range(n):
-      # for v, w in graph[u]:
-      #   curr_dist = distances[u] + w
-      for v in range(n):
-        curr_dist = inf
-        if graph[u][v] != 0:
-          curr_dist = distances[u] + graph[u][v]
+      for v, w in graph[u]:
+        curr_dist = distances[u] + w
+      # for v in range(n):
+      #   curr_dist = inf
+      #   if graph[u][v] != 0:
+      #     curr_dist = distances[u] + graph[u][v]
 
         if curr_dist < distances[v]:
           distances[v] = curr_dist
           parents[v] = u
+
+  # print(verification(graph, distances))
 
   return distances
 
@@ -33,11 +51,21 @@ graph = [[[5, 8], [2, 10]],
          [[4, 1]]]
 
 # [0, 5, 5, 7, 9, 8]
-graph = [[0, 0, 10, 0, 0, 8],
-         [0, 0, 0, 2, 0, 0],
-         [0, 1, 0, 0, 0, 0],
-         [0, 0, -2, 0, 0, 0],
-         [0, -4, 0, -1, 0, 0],
-         [0, 0, 0, 0, 1, 0]]
+# graph = [[0, 0, 10, 0, 0, 8],
+#          [0, 0, 0, 2, 0, 0],
+#          [0, 1, 0, 0, 0, 0],
+#          [0, 0, -2, 0, 0, 0],
+#          [0, -4, 0, -1, 0, 0],
+#          [0, 0, 0, 0, 1, 0]]
+
+# verification: True
+# graph = [[[1, -1], [2, -1]], 
+#          [[0, -1]],
+#          [[0, 1]]]
+
+# verification: True
+# graph = [[0, -1, -1],
+#          [-1, 0, 0],
+#          [1, 0, 0]]
 
 print(bellman_ford(graph, 0))
