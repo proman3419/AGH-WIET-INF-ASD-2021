20bda1b88f6704d4ff9896b7ccf63b02449a7a59
proman3419 <proman3419@protonmail.com>
2021-05-30 13:44:01 +0200

    Fixed bellman-ford

--- cheatsheet/graphs/bellman-ford.py
+++ cheatsheet/graphs/bellman-ford.py
@@ -22,13 +22,13 @@ def get_path(distances, parents, v):
 def verification(graph, distances):
   n = len(graph)
   for u in range(n):
-    for v, w in graph[u]:
-      if distances[v] > distances[u] + w:
-        return True
-    # for v in range(n):
-    #   if graph[u][v] != 0:
-    #     if distances[v] > distances[u] + graph[u][v]:
-    #       return True
+    # for v, w in graph[u]:
+    #   if distances[v] > distances[u] + w:
+    #     return True
+    for v in range(n):
+      if graph[u][v] != 0:
+        if distances[v] > distances[u] + graph[u][v]:
+          return True
 
   return False
 
@@ -42,47 +42,56 @@ def bellman_ford(graph, s):
 
   for i in range(n):
     for u in range(n):
-      for v, w in graph[u]:
-        curr_dist = distances[u] + w
-      # for v in range(n):
-      #   curr_dist = inf
-      #   if graph[u][v] != 0:
-      #     curr_dist = distances[u] + graph[u][v]
+      # for v, w in graph[u]:
+      #   curr_dist = distances[u] + w
+      for v in range(n):
+        curr_dist = inf
+        if graph[u][v] != 0:
+          curr_dist = distances[u] + graph[u][v]
 
-        if curr_dist < distances[v]:
-          distances[v] = curr_dist
-          parents[v] = u
+          if curr_dist < distances[v]:
+            distances[v] = curr_dist
+            parents[v] = u
+
+  if verification(graph, distances):
+    return None
 
   # print(get_path(distances, parents, n-1))
-  # print(verification(graph, distances))
 
   return distances
 
 
 # [0, 5, 5, 7, 9, 8]
-graph = [[[5, 8], [2, 10]],
-         [[3, 2]],
-         [[1, 1]],
-         [[2, -2]],
-         [[1, -4], [3, -1]],
-         [[4, 1]]]
+# graph = [[[5, 8], [2, 10]],
+#          [[3, 2]],
+#          [[1, 1]],
+#          [[2, -2]],
+#          [[1, -4], [3, -1]],
+#          [[4, 1]]]
 
 # [0, 5, 5, 7, 9, 8]
-# graph = [[0, 0, 10, 0, 0, 8],
-#          [0, 0, 0, 2, 0, 0],
-#          [0, 1, 0, 0, 0, 0],
-#          [0, 0, -2, 0, 0, 0],
-#          [0, -4, 0, -1, 0, 0],
-#          [0, 0, 0, 0, 1, 0]]
+graph = [[0, 0, 10, 0, 0, 8],
+         [0, 0, 0, 2, 0, 0],
+         [0, 1, 0, 0, 0, 0],
+         [0, 0, -2, 0, 0, 0],
+         [0, -4, 0, -1, 0, 0],
+         [0, 0, 0, 0, 1, 0]]
 
-# verification: True
+# None
 # graph = [[[1, -1], [2, -1]], 
 #          [[0, -1]],
 #          [[0, 1]]]
 
-# verification: True
+# None
 # graph = [[0, -1, -1],
 #          [-1, 0, 0],
 #          [1, 0, 0]]
+
+# None
+# graph = [[[1, -4]],
+#          [[2, -5]],
+#          [[0, -2]],
+#          [[4, 2]],
+#          [[2, 1]]]
 
 print(bellman_ford(graph, 0))
