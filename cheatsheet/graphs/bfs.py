aa6da6daa4804609f98eb5adcdfa5e8b2680d5d2
proman3419 <proman3419@protonmail.com>
2021-06-05 01:19:16 +0200

    Separated implementations, added time complexity to BFS

--- cheatsheet/graphs/bfs.py
+++ cheatsheet/graphs/bfs.py
@@ -1,53 +1,80 @@
 from collections import deque
 
 
-def bfs(graph, s):
+# O(V^2)
+def bfs_am(graph, s):
   n = len(graph)
   queue = deque()
   visited = [False]*n
   # parents = [None]*n
   # distances = [-1]*n
 
-  # print(f'visit: {s}\n')
   # distances[s] = 0
   visited[s] = True
   queue.append(s)
 
   while queue:
     u = queue.popleft()
-    # print(f'parent: {u}')
-    # for v in range(n):     
-    #   if graph[u][v] == 1 and not visited[v]:
-    for v in graph[u]:
-      if not visited[v]:
-        # print(f'visit: {v}')
+
+    for v in range(n):     
+      if graph[u][v] == 1 and not visited[v]:
         # parents[v] = u
         # distances[v] = distances[u] + 1
         visited[v] = True
         queue.append(v)
-    # print()
+
+  return visited
 
 
-graph = [[1, 2],
-         [0, 3, 4],
-         [0, 5, 6],
-         [1],
-         [1],
-         [2, 7],
-         [2, 8, 9],
-         [5],
-         [6],
-         [6]]
+graph_am = [[0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+            [1, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+            [1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
+            [0, 0, 1, 0, 0, 0, 0, 0, 1, 1],
+            [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 1, 0, 0, 0]]
 
-# graph = [[0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-#          [1, 0, 0, 1, 1, 0, 0, 0, 0, 0],
-#          [1, 0, 0, 0, 0, 1, 1, 0, 0, 0],
-#          [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-#          [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-#          [0, 0, 1, 0, 0, 0, 0, 1, 0, 0],
-#          [0, 0, 1, 0, 0, 0, 0, 0, 1, 1],
-#          [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-#          [0, 0, 0, 0, 0, 0, 1, 0, 0, 0],
-#          [0, 0, 0, 0, 0, 0, 1, 0, 0, 0]]
+print(bfs_am(graph_am, 0))
 
-bfs(graph, 0)
+# ==============================================================================
+
+# O(V + E)
+def bfs_nl(graph, s):
+  n = len(graph)
+  queue = deque()
+  visited = [False]*n
+  # parents = [None]*n
+  # distances = [-1]*n
+
+  # distances[s] = 0
+  visited[s] = True
+  queue.append(s)
+
+  while queue:
+    u = queue.popleft()
+
+    for v in graph[u]:
+      if not visited[v]:
+        # parents[v] = u
+        # distances[v] = distances[u] + 1
+        visited[v] = True
+        queue.append(v)
+
+  return visited
+
+
+graph_nl = [[1, 2],
+            [0, 3, 4],
+            [0, 5, 6],
+            [1],
+            [1],
+            [2, 7],
+            [2, 8, 9],
+            [5],
+            [6],
+            [6]]
+
+print(bfs_nl(graph_nl, 0))
