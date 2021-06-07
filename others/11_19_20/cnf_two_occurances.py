881b290c1a7e7869293499b14e0c7c734c43212c
proman3419 <proman3419@protonmail.com>
2021-06-07 16:49:47 +0200

    Finished cnf_two_occurnaces

---
+++ others/11_19_20/cnf_two_occurances.py
@@ -0,0 +1,6 @@
+# przechodzimy po formulach, tam gdzie sa pojedyncze zmienne to mamy dla nich odpowiedz.
+# wstawiamy odczytane wartosci do pozostalych formul i upraszczamy.
+# dla pozostalych stosujemy zasade rezolucji upraszczajac formuly, 
+# jezeli dojdziemy do ~x v x to mamy tautologie i nie rozpatrujemy juz tej formuly.
+# uproszczone formuly posiadaja zmienne, ktorych wartosci znamy lub takie, ktore nie wplywaja na wynik pozostalych formul.
+# wystarczy dobrac pozostale zmienne co nie jest problemem bo formuly nie sa powiazane
