1d5f4d5df3c1ae0c54874637f35a8b0e8d59a518
proman3419 <proman3419@protonmail.com>
2021-06-12 18:29:50 +0200

    Finished meeting

---
+++ others/bit_algo_start_11/meeting.py
@@ -0,0 +1,5 @@
+# 2*Dijkstra, krawedzie zawieraja informacje o dlugosci oraz o typie sciezki:
+# - 0 - dla wszystkich
+# - 1 - dla ludzi < 30 lat
+# - 2 - dla ludzi >= 30 lat
+# rozwiazaniem sa wierzcholki, o min sumarycznym dystansie z obu Dijkstr
