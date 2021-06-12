bd11aa2f467400bb8ca8e62858f3779cadfdf5f8
proman3419 <proman3419@protonmail.com>
2021-06-12 11:01:11 +0200

    Finished heavy_traffic

---
+++ others/bit_algo_start_11/heavy_traffic.py
@@ -0,0 +1,9 @@
+# 1) Q = O(1), E = O(V)
+# 2) Q = O(1), E = O(V^2)
+# 3) Q = O(V), E = O(V)
+# 4) Q = O(V), E = O(V^2)
+
+# 1) Dijkstra na listach sasiedztwa, O(VlogV)
+# 2) Dijkstra na macierzowej, O(V^2)
+# 3) Dijkstra z kazdego na listach sasiedztwa, O(V*VlogV)
+# 4) Floyd-Warshall na macierzowej, O(V^3)
