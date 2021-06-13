c91983202b9ee7937f52367693787a35b02313e0
proman3419 <proman3419@protonmail.com>
2021-06-13 21:50:21 +0200

    Tried to fix bricks, ended up with a wrong solution

--- 11/bricks.py
+++ 11/bricks.py
@@ -4,28 +4,32 @@ class BSTNode:
     self.left = None
     self.right = None
     self.parent = None
+    self.height = 1
 
 
 def add(tree, key, parent=None):
   if tree is None:
     tree = BSTNode(key)
     tree.parent = parent
+    tree.height = parent.height + 1
   # dodawnay klocek nachodzi od:
   # 1. prawej
   # 2. lewej
-  elif (key[0] < tree.key[1] and tree.key[0] < key[1]) or \
-       (tree.key[0] < key[1] and key[0] < tree.key[1]):
+  elif (key[0] <= tree.key[1] and tree.key[0] <= key[1]) or \
+       (tree.key[0] <= key[1] and key[0] <= tree.key[1]):
     tree.key = (min(tree.key[0], key[0]), max(tree.key[1], key[1]))
     tree.right = add(tree.right, key, tree)
   # nie nachodzi
   else:
     tree.left = add(tree.left, key, tree)
+    tree = tree.left
   
   return tree
 
 
 def print_node(node):
   print(f'key: {node.key}')
+  print(f'height: {node.height}')
 
   if node.left is not None:
     print(f'left: {node.left.key}')
@@ -68,13 +72,13 @@ def print_breadth_first(tree):
     children = _children 
 
 
-def get_height(tree):
-  height = 0
-  while tree is not None:
-    height += 1
-    tree = tree.right
+def get_height(tree, height=0):
+  if tree is None:
+    return height
 
-  return height
+  height = max(height, tree.height)
+
+  return max(get_height(tree.left, height), get_height(tree.right, height))
 
 
 def bricks(ranges):
@@ -92,7 +96,10 @@ ranges = [(1, 3), (2, 5), (0, 3), (8, 9), (4, 6)]
 # 4
 ranges = [(1, 3), (1, 5), (0, 5), (0, 6)]
 
-# 3
+# 4 a powinno byc 3
 ranges = [(0, 1), (2, 3), (0, 3), (0, 1)]
+
+# 6
+ranges = [(1, 3), (2, 5), (0, 3), (8, 9), (4, 6), (4, 6), (4, 6), (0, 10)]
 
 print(bricks(ranges))
