d23ed5ba3009936292af0a1be3ebcf9f3df6d7c1
proman3419 <proman3419@protonmail.com>
2021-06-23 18:36:29 +0200

    Renamed old problems, finished make_pairs_min_sum

---
+++ others/bit_algo_start_2/make_pairs_min_sum.py
@@ -0,0 +1,45 @@
+def partition(A, l, r):
+  x = A[r]
+  i = l - 1
+  for j in range(l, r):
+    if A[j] < x:
+      i += 1
+      A[i], A[j] = A[j], A[i]
+
+  i += 1
+  A[i], A[r] = A[r], A[i]
+
+  return i
+
+
+# zakladamy optymalny podzial za pomoca partition (co najmniej 1 elem w kazdym przedziale)
+# rekurencja i petla lacznie wykonaja sie O(logn) razy
+# na kazdym etapie sortujemy lacznie n elementow, zatem:
+# O(nlogn)
+def quick_sort(A, l, r):
+  while l < r:
+    pi = partition(A, l, r)
+    
+    if pi - l < r - pi:
+      quick_sort(A, l, pi-1)
+      l = pi + 1
+    else:
+      quick_sort(A, pi+1, r)
+      r = pi - 1
+
+  return A
+
+
+# bierzemy min i max liczby do par
+def make_pairs_min_sum(A):
+  n = len(A)
+  A = quick_sort(A, 0, n-1)
+
+  res = [(A[i], A[-i-1]) for i in range(n//2)]
+
+  return res
+
+
+A = [1, 3, 5, 9]
+
+print(make_pairs_min_sum(A))
