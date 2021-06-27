53eb34352fd4b875ceeb26e3b7103e7660668b98
proman3419 <proman3419@protonmail.com>
2021-06-27 12:55:45 +0200

    Fixed frog

--- 6/frog.py
+++ 6/frog.py
@@ -6,30 +6,44 @@ def frog(A):
 
   # F[i][e] - min liczba skokow by dotrzec do i z zapasem e energii
   # mozemy e ograniczyc przez n poniewaz gdy mamy >= n energii to w 1 skoku mozemy osiagnac cel
-  F = [[inf for _ in range(n)] for _ in range(n)]
+  F = [[inf for e in range(n+1)] for i in range(n)]
 
-  for i in range(A[0]+1):
+  for i in range(min(A[0]+1, n+1)):
     F[0][i] = 0
 
-  for i in range(1, A[0]+1):
-    for e in range(A[0]+1-i):
-      F[i][e] = 1
+  def rec(i, e):
+    nonlocal A, F
 
-  for i in range(1, n):
-    for e in range(n):
-      for k in range(1, i):
-        F[i][e] = min(F[i][e], F[i-k][min(max(0, e+k-A[i]), n-k)]) + 1
+    if F[i][e] != inf:
+      return F[i][e]
 
-  return F[n-1][0]
+    for k in range(i):
+      _e = min(e+(i-k)-A[i], n)
+      if e >= i-k and 0 <= _e <= n:
+        F[i][e] = min(F[i][e], rec(k, _e)+1)
+
+    return F[i][e]
+
+  _min = inf
+  for e in range(n+1):
+    _min = min(_min, rec(n-1, e))
+
+  return _min
 
 
+# 3
+A = [1, 2, 3, 4, 5]
 
-A = [1,2,3,4,5]
-# 2
-# A = [1,2,2,2,2,2]
+# 3
+A = [1, 2, 2, 2, 2, 2]
+
 # inf
-# A = [2,2,1,0,0,0]
+A = [2, 2, 1, 0, 0, 0]
+
 # 2
-# A = [4,5,2,5,1,2,1,0]
+A = [4, 5, 2, 5, 1, 2, 1, 0]
+
+# 4
+A = [2, 2, 1, 0, 0]
 
 print(frog(A))
