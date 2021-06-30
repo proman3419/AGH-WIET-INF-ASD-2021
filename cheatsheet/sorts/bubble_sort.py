b7a0ceeeb7587223fd093497d1f8ae90850e8dd7
proman3419 <proman3419@protonmail.com>
2021-06-30 14:13:52 +0200

    Fixed bubble_sort

--- cheatsheet/sorts/bubble_sort.py
+++ cheatsheet/sorts/bubble_sort.py
@@ -2,16 +2,11 @@
 def bubble_sort(A):
   n = len(A)
   for i in range(n): # O(n)
-    swp = False
-    for j in range(i+1, n): # O(n-i) <= O(n)
-      if A[i] > A[j]:
+    for j in range(n): # O(n)
+      if A[i] < A[j]:
         A[i], A[j] = A[j], A[i]
-        swp = True
-
-    if not swp:
-      break
 
   return A
 
 
-print(bubble_sort([15, -2, 1, 0, 6, 3]))
+print(bubble_sort([5, 1, 2, 3, 4]))
