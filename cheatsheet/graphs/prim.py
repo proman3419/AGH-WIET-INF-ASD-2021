c98c19e2f603468cf509534bbf47acf3da245baf
proman3419 <proman7268@gmail.com>
2021-08-12 19:43:30 +0200

    Finished prim

--- cheatsheet/graphs/prim.py
+++ cheatsheet/graphs/prim.py
@@ -3,42 +3,36 @@ from math import inf
 
 
 # O(V^2*log(V))
-def prim_am(graph):
+def prim_am(graph, s=0):
   n = len(graph)
   queue = PriorityQueue()
-  processed = [False]*n
   in_mst = [False]*n
-  parents = [None]*n
 
-  u = 0 # poczatkowy wierzcholek
+  in_mst[s] = True # startujemy w s wiec odznaczamy
   T = [] # MST
-  in_mst[u] = True
 
   for v in range(n):
-    if graph[u][v] != 0:
-      queue.put((graph[u][v], v))
-      parents[v] = u
+    if graph[s][v] != 0:
+      queue.put((graph[s][v], v, s))
 
   while not queue.empty():
-    w, u = queue.get()
+    w, u, p = queue.get()
 
     if not in_mst[u]:
-      T.append([parents[u], u, w])
+      T.append([p, u, w])
       in_mst[u] = True
 
-    if not processed[u]:
+      if len(T) == n - 1:
+        return T
+
       for v in range(n):
         if graph[u][v] != 0:
-          if parents[v] is None or graph[u][v] < graph[parents[v]][v]: 
-            queue.put((graph[u][v], v))
-            parents[v] = u
+          if not in_mst[v]: # jezeli jest juz w MST to krawedzie wychodzace
+          # sa w kolecje lub zostaly juz przetworzone
+            queue.put((graph[u][v], v, u))
 
-      processed[u] = True
-
-  if len(T) != n - 1:
-    # graf niespojny
-    return None
-  return T
+  # graf niespojny
+  return None
 
 
 # [[0, 2, 2], [0, 5, 3], [2, 4, 5], [4, 3, 4], [3, 1, 1]]
@@ -63,37 +57,51 @@ print(prim_am(graph_am))
 # ==============================================================================
 
 # O(E*log(V))
-# def prim_nl(graph):
-#   n = len(graph)
-#   queue = PriorityQueue()
-#   processed = [False]*n
-#   in_mst = [False]*n
-#   parents = [None]*n
+def prim_nl(graph, s=0):
+  n = len(graph)
+  queue = PriorityQueue()
+  in_mst = [False]*n
 
-#   u = 0 # poczatkowy wierzcholek
-#   T = [] # MST
-#   in_mst[u] = True
+  in_mst[s] = True # startujemy w s wiec odznaczamy
+  T = [] # MST
 
-#   for v, w in graph[u]:
-#     queue.put((w, v))
-#     parents[v] = u
+  for v, w in graph[s]:
+    queue.put((w, v, s))
 
-#   while not queue.empty():
-#     w, u = queue.get()
+  while not queue.empty():
+    w, u, p = queue.get()
 
-#     if not in_mst[u]:
-#       T.append([parents[u], u, w])
-#       in_mst[u] = True
+    if not in_mst[u]:
+      T.append([p, u, w])
+      in_mst[u] = True
 
-#     if not processed[u]:
-#       for v, w in graph[u]:
-#         if parents[v] is None or w < graph[parents[v]][v]: 
-#           queue.put((graph[u][v], v))
-#           parents[v] = u
+      if len(T) == n - 1:
+        return T
 
-#       processed[u] = True
+      for v, w in graph[u]:
+        if not in_mst[v]: # jezeli jest juz w MST to krawedzie wychodzace
+        # sa w kolecje lub zostaly juz przetworzone
+          queue.put((w, v, u))
 
-#   if len(T) != n - 1:
-#     # graf niespojny
-#     return None
-#   return T
+  # graf niespojny
+  return None
+
+
+# [[0, 2, 2], [0, 5, 3], [2, 4, 5], [4, 3, 4], [3, 1, 1]]
+graph_nl = [[[1, 7], [2, 2], [3, 8], [5, 3]],
+            [[0, 7], [3, 1]],
+            [[0, 2], [4, 5]],
+            [[0, 8], [1, 1], [4, 4], [5, 12]],
+            [[2, 5], [3, 4], [5, 6]],
+            [[0, 3], [3, 12], [4, 6]]]
+
+# [[0, 6, 1], [6, 3, 1], [3, 2, 2], [2, 1, 3], [3, 4, 3], [6, 5, 5]]
+graph_nl = [[[1, 4], [6, 1]],
+            [[0, 4], [2, 3]],
+            [[1, 3], [3, 2], [6, 10]],
+            [[2, 2], [4, 3], [5, 6], [6, 1]],
+            [[3, 3]],
+            [[3, 6], [6, 5]],
+            [[0, 1], [2, 10], [3, 1], [5, 5]]]
+
+print(prim_nl(graph_nl))
