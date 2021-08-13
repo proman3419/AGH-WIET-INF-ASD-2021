8ac403713c7efdf21a1c44a5abf9ff7cc29faefa
proman3419 <proman7268@gmail.com>
2021-08-14 00:18:42 +0200

    Finished safe_flight

---
+++ 8/safe_flight.py
@@ -0,0 +1,7 @@
+# 1. szukamy min i max pulapu (min_p, max_p) z wierzcholkow dostepnych bezposrednio z x i y
+# 2. wybieramy h = min_p jako obecny pulap i wywolujemy dla niego BFS/DFS, w ktorych
+# uwzgledniamy warunek na pulap tj. abs(h - pi) <= t
+# 3. jezeli BFS/DFS zakonczy sie bez odwiedzenia y to h += 1, jezeli h > max_p to
+# stwierdzamy, ze sciezka nie istnieje
+
+# O((max_p-min_p+1)*V^2)
