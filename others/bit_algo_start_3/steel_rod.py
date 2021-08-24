d7286d67046a1c4220a56f866232e28f858d858a
proman3419 <proman7268@gmail.com>
2021-08-24 22:52:14 +0200

    Added printing solution for steel_rod

--- others/bit_algo_start_3/steel_rod.py
+++ others/bit_algo_start_3/steel_rod.py
@@ -1,19 +1,40 @@
+def get_solution(P):
+  def rec(i):
+    nonlocal P
+
+    if P[i] == -1:
+      return [i]
+
+    return rec(P[i]) + [i-P[i]]
+
+  return rec(len(P)-1)
+
+
 def best_sell(profits):
   n = len(profits)
   # F[i] - max zysk do i
   F = [0]*n
+  P = [-1]*n
   F[0] = profits[0]
 
   for i in range(1, n):
     F[i] = profits[i]
     for j in range(i):
-      F[i] = max(F[i], F[i-j] + profits[j])
+      new_p = F[i-j] + profits[j]
+
+      if new_p > F[i]:
+        F[i] = new_p
+        P[i] = i - j
+
+  print(get_solution(P))
 
   return F[n-1]
 
 
 # profits[i] - zysk ze sprzedazy kawalka o dlugosci i
 # profits[0] = 0, zeby problem mial sens
-profits = [0, 7, 22, 35, 42, 51, 57]
+profits = [0, 5, 15, 23, 33, 39, 45, 51, 60, 71]
 
+# [5, 4]
+# 72
 print(best_sell(profits))
